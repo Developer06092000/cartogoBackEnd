6a94a9acad886098ce6570f735c34537501a32ff
Jalolov_Manguberdi <62201299+Developer06092000@users.noreply.github.com>
2023-06-24 08:19:36 +0500

    123

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const swaggerJsDoc = require("swagger-jsdoc");
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
+app.use("/app/media/", express.static("./app/media/"));
+
 async function sequelize() {
     await db.sequelize
         .sync()
